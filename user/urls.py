277252007e265d3d32516334bb1363d42ab2b387
Divyansh <divyanshsinghrlb@gmail.com>
2024-10-08 23:21:14 +0530

    final commit

--- user/urls.py
+++ user/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
-from .views import shows,add_movie,movie_list,AddShowView
+from .views import shows,add_movie,AddShowView,home,MovieListView
 urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('shows/', shows, name='shows'),
+    path('movie_list/',MovieListView.as_view(), name='movie_list'),
     path('add_movie/', add_movie, name='add_movie'),
-    path('movie_list/', movie_list, name='movie_list'),
     path('add_show/', AddShowView.as_view(), name='AddShowView'),
     
     
--- user/views.py
+++ user/views.py
@@ -1,30 +1,77 @@
+import requests
 from django.shortcuts import render, redirect
 from .models import Show, Movies
 from django.utils import timezone
 from .forms import ShowForm
 from django.views import View
+from django.core.files import File
+from django.core.files.temp import NamedTemporaryFile
 
 def shows(request):
-    upcoming_shows = Show.objects.all().order_by('-time')
-    return render(request, 'shows.html', {'shows': upcoming_shows})
+    shows = Show.objects.all().order_by('-time')
+    return render(request, 'shows.html', {'shows': shows})
 
+def home(request):
+    return render(request, 'base.html')
+# it will fetch data from api if only title field is  provided 
 def add_movie(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         description = request.POST.get('description')
-        poster = request.FILES.get('poster')
-        available = request.POST.get('available', False)
-        movie = Movies.objects.create(title=title, description=description, 
-poster=poster, available=available)
-        print(movie)
-        movie.save
-        
-        return redirect('movie_list')
+        poster = request.FILES.get('poster') 
+        available = request.POST.get('available', False) 
+
+        # only title is provided  fetch data from the API
+        if title and not description and not poster:
+            api_key = # add it 
+            url = f'http://www.omdbapi.com/?t={title}&apikey={api_key}'
+            response = requests.get(url)
+            data = response.json()
+
+            if data['Response'] == 'True':
+                description = data.get('Plot', 'No description available.')
+                poster_url = data.get('Poster') 
+
+                # Downloading the image 
+                img_temp = NamedTemporaryFile()
+                img_response = requests.get(poster_url)
+                img_temp.write(img_response.content)
+                img_temp.flush()
+
+                # Creating  the movie object
+                movie = Movies.objects.create(
+                    title=title,
+                    description=description,
+                    available=available
+                )
+                movie.poster.save(f"{title}_poster.jpg", File(img_temp)) 
+                movie.save()
+
+                return redirect('user:movie_list') 
+            
+            else:
+                # movie not found
+                error_message = data.get('Error', 'Movie not found.')
+                return render(request, 'add_movie.html', {'error_message': error_message})
+
+    
+        else:
+            movie = Movies.objects.create(
+                title=title,
+                description=description,
+                poster=poster, 
+                available=available
+            )
+            movie.save()
+            return redirect('user:movie_list')
+
     else:
         return render(request, 'add_movie.html')
 
+
 def movie_list(request):
     movies = Movies.objects.all()
+      
     return render(request, 'movie_list.html', {'movies': movies})
 
 
@@ -42,3 +89,8 @@ class AddShowView(View):
             form.save()
             return redirect('user:shows')
         return render(request, self.template_name, {'form': form})
+
+class MovieListView(View):
+    def get(self, request):
+        movies = Movies.objects.all() 
+        return render(request, 'movie_list.html', {'movies': movies}) 
