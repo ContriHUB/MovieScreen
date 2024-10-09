bc8dfee2000a14e0584ae4a0a2d8463a40958119
Shoaib samim <samimshoaib01@gmail.com>
2024-10-09 23:04:30 +0530

    updated URLS

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
-from .views import shows,add_movie,movie_list,AddShowView
+from .views import shows,add_movie,movie_list,AddShowView,book_ticket
 urlpatterns = [
     
     path('admin/', admin.site.urls),
@@ -10,6 +10,7 @@ urlpatterns = [
     path('add_movie/', add_movie, name='add_movie'),
     path('movie_list/', movie_list, name='movie_list'),
     path('add_show/', AddShowView.as_view(), name='AddShowView'),
+     path('book_ticket/<int:show_id>/', book_ticket, name='book_ticket'),
     
     
 ]
