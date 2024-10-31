a3cdee6a13d2fc955dc3cccc4ed14440e9fdfac8
Aachintya <aachintya.20233001@mnnit.ac.in>
2024-10-31 10:32:00 +0530

    Redirect Landing URL

--- movie/urls.py
+++ movie/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/user/shows/', permanent=False), name='root'),
     path('admin/', admin.site.urls),
     path('user/', include(('user.urls', 'user'), namespace='user')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
